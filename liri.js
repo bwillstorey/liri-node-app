83b4a7b6369134c338f8e535a1d41ae3951e85ac
bwillstorey <bwillstorey@gmail.com>
2019-03-29 23:27:01 -0500

    movie-this completed

--- liri.js
+++ liri.js
@@ -1,25 +1,29 @@
+var command = process.argv[2];
+var query = process.argv[3];
+
 // concert-this
-var axios = require("axios");
+
+var concertThis = function() {
+
+    var axios = require("axios");
 var moment = require("moment");
 
-var nodeArgs = process.argv;
+var concertArgs = process.argv;
 
 var artist = "";
 
-for (var i = 2; i < nodeArgs.length; i++) {
+for (var i = 2; i < concertArgs.length; i++) {
 
-    if (i > 2 && i < nodeArgs.length) {
-      artist = artist + "+" + nodeArgs[i];
+    if (i > 2 && i < concertArgs.length) {
+      artist = artist + "+" + concertArgs[i];
     }
     else {
-      artist += nodeArgs[i];
+      artist += concertArgs[i];
   
     }
   }
 
 var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-
-// console.log(queryUrl);
 
 axios.get(queryUrl)
 .then(function(response) {
@@ -31,25 +35,109 @@ axios.get(queryUrl)
         var dateFormat = "YYYY-MM-DDTHH:mm:ssZ";
         var convertedDate = moment(rawDate, dateFormat);
         var formattedDate = convertedDate.format("MM/DD/YYYY");
+
         console.log("Event Date: " + formattedDate);
 })
 
 .catch(function(error) {
 
 });
+};
+
+
 
 
 // spotify-this-song
 
+var spotifyThisSong = function() {
 
-// var keys = require("./keys.js");
+    // var keys = require("./keys.js");
 
-// var spotify = new Spotify(keys.spotify);
+    // var spotify = new Spotify(keys.spotify);
+};
+
+
 
 
 // movie-this
 
+var movieThis = function() {
 
-// do-what-it-says
+    var movieArgs = process.argv;
+
+
+var movieName = "";
+
+for (var i = 2; i < movieArgs.length; i++) {
+
+    if (i > 2 && i < movieArgs.length) {
+      movieName = movieName + "+" + movieArgs[i];
+    }
+    else {
+      movieName += movieArgs[i];
+  
+    }
+  }
+
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+axios.get(queryUrl)
+.then(function(response) {
+    console.log("Title: " + response.data.Title);
+    console.log("Year Released: " + response.data.Year);
+    console.log("IMDB Rating: " + response.data.imdbRating);
+    console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
+    console.log("Country: " + response.data.Country);
+    console.log("Language: " + response.data.Language);
+    console.log("Actors: " + response.data.Actors);
+})
+
+.catch(function(error) {
+
+});
+
+};
+
 
 require("dotenv").config();
+
+
+if(command === "concert-this") {
+	concertThis(query);
+} else if(command === "spotify-this-song") {
+	spotifyThisSong(query);
+} else if(command === "movie-this") {
+	movieThis(query);
+} else if(command === "do-what-it-says") {
+    
+    // do-what-it-says
+    var fs = require("fs");
+
+	fs.readFile("random.txt", "utf-8", function(error, data) {
+		var command;
+		var query;
+
+		if(data.indexOf(",") !== -1) {
+			var dataArr = data.split(",");
+			command = dataArr[0];
+			query = dataArr[1];
+		} else {
+			command = data;
+		}
+
+		
+		if(command === "concert-this") {
+			concertThis(query);
+		} else if(command === "spotify-this-song") {
+			spotifyThisSong(query);
+		} else if(command === "movie-this") {
+			movieThis(query);
+		} else { 
+			console.log("Command from file is not a valid command! Please try again.");
+		}
+	});
+} else if(command === undefined) {
+	console.log("LIRI needs a command to run.");
+} else {
+	console.log("Error. Please try again.");
+}
