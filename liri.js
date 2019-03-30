132f3f17f0e49970ab3d8dc7640badf1da48eddb
bwillstorey <bwillstorey@gmail.com>
2019-03-29 21:47:09 -0500

    partial concert working

--- liri.js
+++ liri.js
@@ -1,16 +1,48 @@
-require("dotenv").config();
-
-var keys = require("./keys.js");
-
-var spotify = new Spotify(keys.spotify);
-
 // concert-this
+var axios = require("axios");
+
+var nodeArgs = process.argv;
+
+var artist = "";
+
+for (var i = 2; i < nodeArgs.length; i++) {
+
+    if (i > 2 && i < nodeArgs.length) {
+      artist = artist + "+" + nodeArgs[i];
+    }
+    else {
+      artist += nodeArgs[i];
+  
+    }
+  }
+
+var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+console.log(queryUrl);
+
+axios.get(queryUrl)
+.then(function(response) {
+        console.log("Venue Name: " + response.data[0].venue.name);
+        console.log("Venue Location: " + response.data[0].venue.city + ", " + response.data[0].venue.region + ", " + response.data[0].venue.country);
+        console.log("");
+})
+
+.catch(function(error) {
+
+});
 
 
 // spotify-this-song
+
+
+// var keys = require("./keys.js");
+
+// var spotify = new Spotify(keys.spotify);
 
 
 // movie-this
 
 
 // do-what-it-says
+
+require("dotenv").config();
