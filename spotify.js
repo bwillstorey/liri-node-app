3a6116dcd3db60a8a7ffa90205f50f1018e7efb6
bwillstorey <bwillstorey@gmail.com>
2019-03-31 16:37:43 -0500

    working spotify module

--- spotify.js
+++ spotify.js
@@ -9,19 +9,26 @@
 
 function spotify (song) {
     
+    if (song === "") {
+        return spotify("the sign");
+    }
+
     runSpotify.search({
         type: "track",
         query: song,
     })
     .then(function(response) {
+        // console.log(response.tracks.items[0]);
 
-        if (song === undefined) {
-            song = "the sign";
+        for (var i = 0; i < 3; i++) {
+            var resp = response.tracks.items[i];
+            
+            console.log("\n------------------------");
+            console.log("Artist: " + resp.album.artists[0].name);
+            console.log("Song Name: " + resp.name);
+            console.log("Preview: " + resp.preview_url);
+            console.log("Album: " + resp.album.name);
         }
-        else {
-            spotify("the sign");
-        }
-
     })
 
     .catch(function(error) {
